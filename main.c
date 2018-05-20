53eecf2178bc35cd77927b68def8ef1d96c77c40
Tiziano Martin hernando <tizianomartinhernando@MacBook-Pro-de-Tiziano.local>
2018-05-20 11:49:54 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10] ={1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    printf("%p\n", &a[1]);// Sabemos en que sitio de la memoria esta el arreglo
+
+
+
+    //Comprobacion
+
+    /*
+    int x, *p, *q;
+    x = 5, p = &x, q = p;
+    printf("\n %p\n %p\n %p\n", p, q, &x);
+
+    */
+
+
+
+
+return 0;
+}
